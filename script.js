734334066bcdb4960f25db31fd5773d01e0947cf
Andwerpz <andwerpooni@gmail.com>
2022-03-04 10:58:02 -0600

    added upgrades menu

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let jacobs = 0;
+
+let factoryBuilt = false;
 let factoryJacobs = 0;
 let factoryJacobRate = 0.05;
+
+let schoolBuilt = false;
 let schoolJacobs = 0;
 
 let prevUpdateTime = Date.now();
@@ -12,9 +16,13 @@ function loadGame(){
   if(localStorage.getItem("gameSave") != null){
     let savedValues = JSON.parse(localStorage.getItem("gameSave"));
     jacobs = parseFloat(savedValues.jacobs);
-    factoryJacobs = savedValues.factoryJacobs;
-    factoryJacobRate = savedValues.factoryJacobRate;
-    schoolJacobs = savedValues.schoolJacobs;
+    
+    factoryBuilt = savedValues.factoryBuilt;
+    factoryJacobs = parseFloat(savedValues.factoryJacobs);
+    factoryJacobRate = parseFloat(savedValues.factoryJacobRate);
+    
+    schoolBuilt = savedValues.schoolBuilt;
+    schoolJacobs = parseFloat(savedValues.schoolJacobs);
   }
   else{
     saveGame();
@@ -24,8 +32,12 @@ function loadGame(){
 function saveGame(){
   let gameSave = {
     jacobs: jacobs,
+    
+    factoryBuilt: factoryBuilt,
     factoryJacobs: factoryJacobs,
     factoryJacobRate: factoryJacobRate,
+    
+    schoolBuilt: schoolBuilt,
     schoolJacobs: schoolJacobs
   }
   localStorage.setItem("gameSave",JSON.stringify(gameSave));
@@ -40,6 +52,7 @@ function reloadCounters() {
     
   document.getElementById("school_jacob_counter").innerHTML = parseInt(schoolJacobs);
 }
+
 function constructJacob() {
   jacobs++;
   reloadCounters();
